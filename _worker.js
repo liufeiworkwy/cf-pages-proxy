0cfae7e29494ad5f83729b4bc03822737d145108
liufeiworkwy <73467020+liufeiworkwy@users.noreply.github.com>
2024-02-20 10:19:37 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,12 @@
 export default {
   async fetch(request, env) {
-    let url = new URL(request.url);
+    let url = new URL(request.url);  
     if (url.pathname.startsWith('/telegraph/')) {
-          url.hostname = 'telegra.ph';
+          url.hostname = 'telegra.ph'；
+          url.pathname = url.pathname.replace("/telegraph/","");
       }else if(url.pathname.startsWith('/telegraph-api/')){
           url.hostname = 'api.telegra.ph';
+          url.pathname = url.pathname.replace("/telegraph-api/","");
       }
       let new_request = new Request(url, request);
       return fetch(new_request);
